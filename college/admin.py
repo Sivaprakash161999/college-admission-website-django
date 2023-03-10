84365b47c67a0a005b1f90fe76159f7a71a51116
Sivaprakash161999 <sivaprakash161999@gmail.com>
2023-03-10 16:55:57 +0530

    added logics

--- college/admin.py
+++ college/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .model import Application, Notice, Detail
 
 # Register your models here.
+admin.site.register(Application)
+admin.site.register(Notice)
+admin.site.register(Detail)
